f532187173c7ea71e492c2f08580647597100df4
Diego José de Melo <diegomeloads@gmail.com>
2026-03-11 23:00:43 -0300

    adjust rated pylint

--- analytics-service/app.py
+++ analytics-service/app.py
@@ -13,7 +13,7 @@ from azure.data.tables import TableServiceClient, UpdateMode
 from azure.core.exceptions import ResourceExistsError
 
 # ---------------------------------------------------------
-# Loggingss
+# Loggings
 # ---------------------------------------------------------
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
